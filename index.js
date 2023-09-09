e52f4d052925e6c2bc7539d1e436f2db1892a3e1
moorthy <mutharasu2000agpt@gmail.com>
2023-09-08 23:32:52 -0700

    Async Task

---
+++ index.js
@@ -0,0 +1,26 @@
+const cardContainer = async ()=>{
+    try{
+        const res =await fetch("https://jsonplaceholder.typicode.com/users");
+        const result = await res.json();;
+       
+         const cardCollection= document.getElementById('cardCollection');
+        for(var i=0;i<result.length; i++){
+                const divChild = document.createElement("div");
+                
+                divChild.innerHTML=`
+                <div class="card-body">
+                  <h5 class="card-title">Name : ${result[i].name}</h5>
+                  <p class="card-text"> User Name : ${result[i].username}</p>
+                  <p class="card-text"> Emailid : ${result[i].emailid}</p>
+                  <p class="card-text"> City: ${result[i].address.city}</p>
+                </div>`
+                
+                cardCollection.append(divChild);
+        }   
+        divChild.setAttribute('class',"child")
+                
+    }catch(error){
+        console.log(console.log(error));
+    }
+}
+cardContainer();
